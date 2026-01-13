4bd5f9f1cc5f964e3a95bd00848577dcf1255e9a
MoeJana <m.eljanati@rubika-edu.ca>
2026-01-12 19:11:29 -0500

    Create pipe_config.py

---
+++ bernardFever/pipe_config.py
@@ -0,0 +1,15 @@
+import os
+asset_list = ['male']
+asset_types = ['characters', 'props']
+project_path = 'C:/Users/danie/OneDrive/Rendermotion/Shortfilm2023/short_film/assets'
+asset_path = '{}'
+model_path = 'modeling'
+rig_path = 'rigging'
+data_path = f'{os.getlogin()}/data'
+publish_folder = '_publish'
+modules_path = 'rigBuilds.assets'
+default_module = 'default_character'
+facial_definition = 'facial_definition'
+geo_definition = 'geo_definition'
+
+
